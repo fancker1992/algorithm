00cf588573ea5f87b75143424df9fb6caf6b1078
大熊 <786087292@qq.com>
2019-12-29 18:58:28 +0800

    新增归并排序

---
+++ sort/mergeSort.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+def merge_sort(alist):
+    n = len(alist)
+    if n == 1:
+        return alist
+    mid = n // 2
+
+    left_list = merge_sort(alist[:mid])
+    right_list = merge_sort(alist[mid:])
+
+    left, right = 0, 0
+    left_n = len(left_list)
+    right_n = len(right_list)
+    merge_list = []
+    while left < left_n and right < right_n:
+        if left_list[left] <= right_list[right]:
+            merge_list.append(left_list[left])
+            left += 1
+        elif left_list[left] > right_list[right]:
+            merge_list.append(right_list[right])
+            right += 1
+    merge_list += left_list[left:]
+    merge_list += right_list[right:]
+    return merge_list
+
+
+if __name__ == '__main__':
+    alist = [23, 6, 34, 55, 76, 1, 0, 11, 5]
+    print(alist)
+
+    print(merge_sort(alist))
